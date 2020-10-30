2aa4f04af5181278b46eb3e7456cb9dcd3901a23
kouroshelkaie <kourosh.elkaie@gmail.com>
2020-10-30 15:56:32 +0330

    copy root

---
+++ script/toggle.js
@@ -0,0 +1,10 @@
+
+let bdy = document.getElementById('bdy');
+let subNav = document.getElementById('chro')
+let navigation = document.getElementById('navi')
+
+
+    // sidebarToggle
+function sidebarToggle() {
+    bdy.classList.toggle('side-toggle')
+}
